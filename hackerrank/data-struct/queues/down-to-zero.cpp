4323e7d9f03b79e0c4abfaac810a4f70dc6a0db9
maximg <maxim.golov@gmail.com>
2017-03-11 17:51:25 +0100

    Short circuit works, result is wrong still
    808707 is 9, should be 8

--- hackerrank/data-struct/queues/down-to-zero.cpp
+++ hackerrank/data-struct/queues/down-to-zero.cpp
@@ -27,23 +27,22 @@ vector<int>& candidate_divisors(int n) {
             ret.push_back(n / i);
     }
     divisors[n] = ret;
-    cout << "divs(" << n << "): ";
-    dump(ret);
+//    cout << "divs(" << n << "): ";
+//    dump(ret);
     return divisors[n];
 }
 
 int trace (int n, int ret, char c, int current_best, int current_step) {
-    cout << n << ": " << ret << " " << c << " " << current_best << " " << current_step << "\n";
+//    cout << n << ": " << ret << " " << c << " " << current_best << " " << current_step << "\n";
     return ret;
 }
 
-// FIXME: need to finish short-circuiting when the search is not useful anymore
 int current_best;
 
 int solve(int n, int current_step, bool allow_sc) {
     if (allow_sc && current_step >= current_best) {
-        cout << "short-sircuit\n";
-        return INT_MAX;
+        //cout << n << ": short-sircuit\n";
+        return current_step;
     }
     
     if (n == 1)
@@ -55,20 +54,28 @@ int solve(int n, int current_step, bool allow_sc) {
         return trace(n, cached, ' ', current_best, current_step);
 
     const auto &divs = candidate_divisors(n);
-    if (divs.empty())
-        return trace(n-1, 1 + solve(n-1, current_step+1, false), '#', current_best, current_step);
+    if (divs.empty()) {
+        int cnt = solve(n-1, current_step+1, false);
+        return trace(n-1, 1 + cnt, '#', current_best, current_step);
+    }
 
     int min_steps = INT_MAX;
-    for (auto k: divs) {
-        min_steps = min(min_steps, solve(k, current_step+1, true));
+    auto solve1 = [&](int k) {
+        int cnt = solve(k, current_step+1, true);
+        if (cnt == INT_MAX)
+            return;
+        min_steps = min(min_steps, cnt);
         if (current_step + min_steps < current_best)
             current_best = current_step + min_steps;
-    }
-    min_steps = min(min_steps, solve(n-1, current_step+1, true));
-    if (current_step + min_steps < current_best)
-        current_best = current_step + min_steps;
-    steps[n] = min_steps + 1;
-    return trace(n, min_steps + 1, '*', current_best, current_step);
+    };
+    
+    for (auto k: divs)
+        solve1(k);
+    solve1(n-1);
+
+    int ret = min_steps == INT_MAX ? min_steps : min_steps + 1;
+    steps[n] = ret;
+    return trace(n, ret, '*', current_best, current_step);
 }
 
 int main() {
