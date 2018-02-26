acbe37935ddc5eebf1b5bc2dd346b62622648688
Maksim Golov <maxim.golov@gmail.com>
2018-02-26 21:27:36 +0100

    Hackerrank: poisonous plants

--- hackerrank/data-struct/stack/poisoned-plants.cpp
+++ hackerrank/data-struct/stack/poisoned-plants.cpp
@@ -1,50 +1,41 @@
 #include <cmath>
 #include <cstdio>
-#include <vector>
+#include <stack>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-// Incorrect solution
-// Does not pass this TC:
-
-//17
-//20 5 6 15 2 2 17 2 11 5 14 5 10 9 19 12 5
+https://www.hackerrank.com/challenges/poisonous-plants/forum
 
 int max_days{0};
-int days{0};
-int tail{0};
-int group_tail{0};
+stack<pair<int, int> > st;
 
 void add(int x) {
-    cout << x << ": ";
-    if (x <= tail) {
-        tail = x;
-        group_tail = x;
-        max_days = max(max_days, days);
-        days = 0;
-        cout << "reset\n";
-        return;
+    if (!st.empty()) {
+        if (x > st.top().first) {
+            st.push({x, 1});
+            max_days = max(max_days, 1);
+            return;
+        }
+        int cmax{0};
+        while (!st.empty() && st.top().first >= x) {
+            cmax = max(cmax, st.top().second);
+            st.pop();
+        }
+        if (!st.empty()) {
+            st.push({x, cmax + 1});
+            max_days = max(max_days, cmax + 1);
+        }
     }
-    if (days == 0) ++days;
-    if (x > group_tail) {
-        group_tail = x;
-    } else {
-        group_tail = tail;
-        ++days;
-    }
-    cout << days << " " << group_tail << endl;
+    if (st.empty()) {
+        st.push({x, 0});
+    }    
 }
 
 int main() {
-    size_t n{0};
-    cin >> n;
-    int x{0};
-    cin >> tail;
-    for (size_t i=1; i < n; ++i) { cin >> x; add(x); }
-    
-    max_days = max(max_days, days);
+    size_t n; cin >> n;
+    for (size_t i=0; i < n; ++i) { int x; cin >> x; add(x); }
+
     cout << max_days << endl;
     return 0;
 }
-
