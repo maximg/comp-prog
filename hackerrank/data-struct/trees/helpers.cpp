119a27393ea7e9fc97047597cdbe497ad68adc4b
Maksim Golov <maxim.golov@gmail.com>
2018-03-02 23:53:18 +0100

    Added hand-rolled queue class to helpers

--- hackerrank/data-struct/trees/helpers.cpp
+++ hackerrank/data-struct/trees/helpers.cpp
@@ -1,4 +1,26 @@
 
+template<typename T>
+class queue {
+public:
+    bool empty() const { return head == tail; }
+    void push(const T& v) {
+        tail = (tail + 1) % MAX_ELEMS;
+        assert(!empty());
+        elems[tail] = v;
+    }
+    void pop() { assert(!empty()); head = (head + 1) % MAX_ELEMS; }
+    T& front() { return elems[head]; }
+private:
+    void assert(bool cond) {
+        if (!cond) { cerr << "assert failed"; exit(-1); }
+    }
+
+    static constexpr size_t MAX_ELEMS = 10000;
+    T elems[MAX_ELEMS];
+    size_t head, tail;
+};
+
+
 void dumpLevelOrder(node * root) {
     queue<node*> q;
     q.push(root); q.push(nullptr);
@@ -13,5 +35,5 @@ void dumpLevelOrder(node * root) {
             if (n->right) q.push(n->right);
             cout << n->data << " ";   
         }
-    }	
+    }
 }
