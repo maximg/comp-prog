3361abc810da8850b8fdd0d49f3bc154153f611f
Maksim Golov <maxim.golov@gmail.com>
2018-03-14 11:40:30 +0100

    Hackerrank: Max index product (unfinished)

---
+++ hackerrank/data-struct/advanced/maximum-index-product.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    size_t n; cin >> n;
+    int64_t index_product{0};
+    
+    if (n > 2) {
+        vector<int> a(n);
+        for (size_t i = 0; i < n; ++i) cin >> a[i];
+
+        // ...
+    }
+    cout << index_product << endl;
+    return 0;
+}
