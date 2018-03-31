deec16c6926753c49a8ce5c5b9beca5d33606a74
Maksim Golov <maxim.golov@gmail.com>
2018-03-31 22:08:04 +0200

    Hackerrank: Kruskal MST Special Subtree (slower but simpler)

--- hackerrank/algorithms/graph-theory/kruskal-special-subtree.cpp
+++ hackerrank/algorithms/graph-theory/kruskal-special-subtree.cpp
@@ -27,39 +27,25 @@ int main()
     }
     sort(edges.begin(), edges.end(), [](const Edge &a, const Edge &b) {
         return (a.weight == b.weight)
-            ? ((a.from + a.to) < (b.from + b.to)) 
+            ? ((a.from + a.weight + a.to) < (b.from + b.weight + b.to)) 
             : a.weight < b.weight;
     });
 
-    int comp{1};
-    int g_weight{0};
     vector<int> comps(g_nodes+1);
+    for (int i = 0; i <= g_nodes; ++i)
+        comps[i] = i;
     
+    int g_weight{0};
     for (int i = 0; i < g_edges; ++i) {
         auto &e = edges[i];
         //cout << e.from << " " << e.to << " " << e.weight << " ";
-        bool new_comp = max(comps[e.from], comps[e.to]) == 0;
-        bool loop = !new_comp && (comps[e.from] == comps[e.to]);
-        bool attach = !loop && !new_comp && (min(comps[e.from], comps[e.to]) == 0);
-        if (loop) {
-            //cout << endl;
-            continue;
-        }
-        if (new_comp) {
-            comps[e.from] = comps[e.to] = ++comp;
-            //cout << comps[e.from]<< " +"  << endl;
-        } else if (attach) {
-            comps[e.from] = comps[e.to] = max(comps[e.from], comps[e.to]);
-            //cout << comps[e.from]<< " *"  << endl;
-        } else {
-            // join 2 subgraphs
+        if (comps[e.from] != comps[e.to]) {
+            g_weight += e.weight;
             int from = comps[e.from];
             for (auto &n: comps) {
                 if (n == from) n = comps[e.to];
             }
-            //cout << from << " -> " << comps[e.to] << endl;
         }
-        g_weight += e.weight;
     }
 
     cout << g_weight << endl;
