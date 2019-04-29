c6e85a57f401433318c49cb0431f2a64cf4683ff
Maksim Golov <maxim.golov@gmail.com>
2019-04-29 06:04:00 +0200

    UvA: 10029 step ladders, accepted

--- uva/10029-edit-step-ladders.cpp
+++ uva/10029-edit-step-ladders.cpp
@@ -1,14 +1,12 @@
 
+// https://uva.onlinejudge.org/external/100/10029.pdf
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <cmath>
 
 using namespace std;
-
-struct Vertex {
-    vector<int> edges;
-};
 
 bool is_edit_step(const string& a, const string& b) {
     if (a.size() - b.size() > 1 && b.size() - a.size() > 1)
@@ -40,9 +38,26 @@ bool is_edit_step(const string& a, const string& b) {
         return diff_count == 1;
 }
 
+vector<string> words;
+vector<int> dp;
+
+int dfs(int i) {
+    if (dp[i] > 0)
+        return dp[i];
+
+    dp[i] = 1;
+    for (int j = i+1; j < words.size(); ++j) {
+        if (is_edit_step(words[i], words[j])) {
+            int longest = 1 + dfs(j);
+            //cout << words[i] << ": " << longest << endl;
+            dp[i] = max(dp[i], longest);
+        }
+    }
+    return dp[i];
+}
+
 int main() {
 
-    vector<string> words;
     while (cin) {
         string str;
         getline(cin, str);
@@ -50,24 +65,12 @@ int main() {
             words.emplace_back(str);
     }
 
-    vector<Vertex> g;
-    g.reserve(words.size());
-    for (int i = 0; i < words.size(); ++i) {
-        g.emplace_back(Vertex{{}});
-        for (int j = i + 1; j < words.size(); ++j) {
-            if (is_edit_step(words[i], words[j]))
-                g.back().edges.emplace_back(j);
-        }
-    }
+    dp.resize(words.size(), -1);
+    int max_ladder = 0;
+    for (int i = 0; i < words.size(); ++i)
+        max_ladder = max(max_ladder, dfs(i));
 
-    for (int i = 0; i < words.size(); ++i) {
-        cout << words[i] << ":";
-        for (const auto& idx: g[i].edges)
-            cout << " " << words[idx];
-        cout << endl;
-    }
-
-    // find connected components and for each find the longest step ladder
+    cout << max_ladder << endl;
 
     return 0;
 }
